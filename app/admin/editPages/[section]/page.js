432c01f055a423ae0d62bf60807a9c72973e9510
dhritiman78 <dhritiman.saikia.11b.244@gmail.com>
2024-07-16 09:56:16 +0530

    new commit

--- app/admin/editPages/[section]/page.js
+++ app/admin/editPages/[section]/page.js
@@ -3,20 +3,23 @@ import React from 'react'
 import AdminNavbar from '@/components/AdminNavbar'
 import Footerbar from '@/components/Footerbar'
 import Intro from '../editPageComponents/Intro'
+import { usePathname } from 'next/navigation';
 import './sectionPage.css'
 import Socials from '../editPageComponents/Socials'
 
-const RenderForms = ({ section }) => {
+const RenderForms = () => {
+  const pathname = usePathname();
+  const section = pathname.split('/')[3];
   switch (section) {
       case 'intro':
           return (<Intro />)
       case 'socials':
             return (<Socials />)
+    
       default:
           return (<div>Not selected</div>)
   }
 }
-
 const page = () => {
   return (
     <>
