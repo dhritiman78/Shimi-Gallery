eaab4c284fe081d91d6d6a883cf230412a4e6944
hardMaster <dauffaummafaku-4104@yopmail.com>
2024-07-16 15:57:01 +0530

    Updated db.js

--- db/db.js
+++ db/db.js
@@ -1,10 +1,20 @@
 import mongoose from "mongoose";
-const { username , password } = process.env
+
+const userName = process.env.NEXT_DB_USERNAME;
+const password = process.env.NEXT_DB_PASSWORD;
+const database = process.env.NEXT_DB_DATABASE;
+
+const uri = `mongodb+srv://${userName}:${password}@cluster0.c4doeee.mongodb.net/${database}`;
+
+const clientOptions = {
+  serverSelectionTimeoutMS: 50000,
+  socketTimeoutMS: 50000,
+};
 
 export async function dbconn() {
-    try {
-       await mongoose.connect('mongodb+srv://'+username+':'+password+'@cluster0.c4doeee.mongodb.net/sharminsart');        
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(uri, clientOptions);
+  } catch (error) {
+    console.log(error);
+  }
 }
