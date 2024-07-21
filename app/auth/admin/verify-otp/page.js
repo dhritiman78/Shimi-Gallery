06e8786e35363a648194f34db6019099e72fcc28
Dhritiman Saikia <147172527+dhritiman78@users.noreply.github.com>
2024-07-21 19:02:31 +0530

    Update page.js

--- app/auth/admin/verify-otp/page.js
+++ app/auth/admin/verify-otp/page.js
@@ -12,7 +12,7 @@ const OtpForm = () => {
   const [isError, setError] = useState(false);
   const [isOtpSent,setIsOtpSent] = useState(false)
   const handleSendOtp = async () => {
-    const email = 'minibarsnathsaikia@gmail.com'
+    const email = 'nikitabaruah74@gmail.com'
     try {
       const response = await fetch('./verify-otp/send-otp',  {
         method: 'POST',
